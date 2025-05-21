b1997a251ac59bbb6c76cccda8031fd3d331a527
Tim Warner <timothywarner316@gmail.com>
2025-05-21 09:00:51 -0500

    add test constant

--- teams-relay.js
+++ teams-relay.js
@@ -1,3 +1,4 @@
+const test = 'test'
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
@@ -6,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 
 // Your Microsoft Teams Incoming Webhook
-const TEAMS_WEBHOOK = 'https://timwinfo2.webhook.office.com/webhookb2/f6b13cc4-328b-4395-baeb-f8632dd229b2@f74b1450-e46a-41df-abee-ebf3621bfd85/IncomingWebhook/f25dcd95ffc14a8f9ec012996ce33608/630743f1-e10f-401c-8c02-379470b66ef7/V2ZATWDb8DgzCVmb4P0kTxFTPwcWliyfBlvBAfFutzASg1';
+const TEAMS_WEBHOOK = '';
 
 app.post('/webhook', async (req, res) => {
   const event = req.headers['x-github-event'];
